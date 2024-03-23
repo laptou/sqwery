bc4839fb7704a6234e540ef8982f3ae7ad5523c1
Ibiyemi Abiodun <ibiyemi@intulon.com>
2024-03-22 20:53:04 -0400

    initial commit

--- Package.swift
+++ Package.swift
@@ -4,20 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "sqwery",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "sqwery",
-            targets: ["sqwery"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "sqwery"),
-        .testTarget(
-            name: "sqweryTests",
-            dependencies: ["sqwery"]),
-    ]
+  name: "sqwery",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "sqwery",
+      targets: ["sqwery"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/hyperoslo/Cache", .upToNextMajor(from: "6.2.0")),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "sqwery",
+      dependencies: [.product(name: "Cache", package: "Cache")]
+    ),
+    .testTarget(
+      name: "sqweryTests",
+      dependencies: ["sqwery"]
+    ),
+  ]
 )
