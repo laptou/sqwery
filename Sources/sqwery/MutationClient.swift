94113bcf99adb3d0bf24d49e589fa2ac10d21b85
Ibiyemi Abiodun <ibiyemi@intulon.com>
2024-09-11 02:50:53 -0400

    mutation client logging

--- Sources/sqwery/MutationClient.swift
+++ Sources/sqwery/MutationClient.swift
@@ -1,5 +1,6 @@
 import Combine
 import Foundation
+import os
 
 public actor MutationClient {
   public static let shared = MutationClient()
@@ -7,20 +8,36 @@ public actor MutationClient {
   private let subject = PassthroughSubject<(mutationKey: AnyHashable, mutationState: Any), Never>()
 
   public func mutate<K: MutationKey>(_ key: K, parameter: K.Parameter) async -> AsyncStream<RequestState<K.Result, K.Progress>> {
+    let logger = Logger(
+      subsystem: "sqwery",
+      category: "mutation \(String(describing: key))"
+    )
+    
     var state = RequestState<K.Result, K.Progress>()
     state.queryStatus = .pending(progress: nil)
     subject.send((AnyHashable(key), state))
+    
+    logger.debug("mutation loop starting")
 
     Task {
       do {
         state.retryCount = 0
 
         while true {
+          if Task.isCancelled {
+            logger.debug("mutation cancelled")
+            break
+          }
+          
           do {
+            logger.trace("running mutation")
+            
             let result = try await key.run(client: self, parameter: parameter, onProgress: { progress in
               state.queryStatus = .pending(progress: progress)
               self.subject.send((AnyHashable(key), state))
             })
+            
+            logger.debug("mutation success")
 
             state.queryStatus = .success(value: result)
             subject.send((AnyHashable(key), state))
@@ -29,17 +46,24 @@ public actor MutationClient {
 
             break
           } catch {
+            
             state.retryCount += 1
 
             if state.retryCount >= key.retryLimit {
               throw error
             }
+            
+            logger.warning("mutation fail, retrying")
 
             try? await Task.sleep(for: key.retryDelay)
           }
         }
 
+      } catch is CancellationError {
+        logger.debug("mutation cancelled")
       } catch {
+        logger.error("mutation fail, giving up")
+        
         state.queryStatus = .error(error: error)
         subject.send((AnyHashable(key), state))
 
