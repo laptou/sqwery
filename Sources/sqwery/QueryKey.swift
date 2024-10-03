5a5b1be5135f9d557f735058dba7085fb671c806
Ibiyemi Abiodun <ibiyemi@intulon.com>
2024-10-03 02:05:31 -0400

    allow optional query keys

--- Sources/sqwery/QueryKey.swift
+++ Sources/sqwery/QueryKey.swift
@@ -17,3 +17,39 @@ public extension QueryKey {
   func onSuccess(client _: QueryClient, result _: Result) async {}
   func onError(client _: QueryClient, error _: Error) async {}
 }
+
+extension Optional: QueryKey where Wrapped: QueryKey {
+  public var resultLifetime: Duration {
+    if let self {
+      return self.resultLifetime
+    }
+    
+    return Duration.seconds(1_000_000_000)
+  }
+  
+  public var retryDelay: Duration {
+    if let self {
+      return self.retryDelay
+    }
+    
+    return Duration.zero
+  }
+  
+  public var retryLimit: Int {
+    if let self {
+      return self.retryLimit
+    }
+    
+    return 0
+  }
+  
+  public func run(client: QueryClient) async throws -> Wrapped.Result? {
+    if let self {
+      return try await self.run(client: client)
+    }
+    
+    return nil
+  }
+  
+  public typealias Result = Wrapped.Result?
+}
